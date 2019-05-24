c7e55e2fb1e16aa8e9047df5980f9abafdb224c3
张亚中 <46549169+pink-pig-pig@users.noreply.github.com>
2019-05-24 18:03:40 +0800

    Add files via upload

--- 学生信息管理系统/studentsSystem.py
+++ 学生信息管理系统/studentsSystem.py
@@ -1,26 +1,37 @@
-
 import re
+#主菜单
 def menu():
-    pass
+    print("【----------------------------学生信息管理系统-------------------------------】")      
+    print("【                             0: 退出该系统                                ")                              
+    print("【                             1：插入学生信息                              ") 
+    print("【                             2：查找学生信息                              ")  
+    print("【                             3：删除学生信息                              ")  
+    print("【                             4：修改学生信息                              ")  
+    print("【                             5：排序                                     ")  
+    print("【                             6：统计                                     ")  
+    print("【                             7：展示所有信息                              ") 
+    print("【         使用中有任何问题，可以重新登录来解决，万能解决方法                   】")                               
+                                    
 
+#插入学生信息
 def insert():
     pass
-
+#查找
 def search():
     pass
-
+#删除
 def delete():
     pass
-
+#修改
 def modify():
     pass
-
+#对学生成绩排序
 def sort():
     pass
-
+#统计
 def total():
     pass
-
+#展示所有信息
 def show():
     pass
 
@@ -55,3 +66,4 @@ def main():
             print("请输入正确的数字！")    
             
         
+main()
